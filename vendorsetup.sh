670e2586ed367e47d0a00857740a604cb6fd9d5b
awana <80144987+awana111@users.noreply.github.com>
2025-05-04 22:21:43 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
-rm -rf android/kernel/asus/sdm660/4.4
-git clone --recursive https://github.com/SonicBSV/android_kernel_asus_sdm660_4.4 -b R android/kernel/asus/sdm660/4.4
+rm -rf kernel/asus/sdm660
+git clone --recursive https://github.com/SonicBSV/android_kernel_asus_sdm660_4.4 -b R kernel/asus/sdm660
 
 rm -rf lineage-18.1 device/asus/sdm660-common-4.4
 git clone --depth=1 https://github.com/SonicBSV/device_asus_sdm660-common-4.4 -b lineage-18.1 device/asus/sdm660-common-4.4
