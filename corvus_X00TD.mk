6dd89ec15a90284d60fb684c9e22dfa30c21790e
awana <80144987+awana111@users.noreply.github.com>
2025-07-17 23:24:46 +0700

    Update and rename lineage_X00TD.mk to corvus_X00TD.mk

--- corvus_X00TD.mk
+++ corvus_X00TD.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, device/asus/X00TD/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := corvus_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
