9786c95ab2c97bc5f5cbd51a3a60f306bbf40474
awana <80144987+awana111@users.noreply.github.com>
2025-07-17 23:16:25 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,10 @@ git clone --depth=1 https://github.com/SonicBSV/device_asus_sdm660-common-4.4 -b
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/SonicBSV/proprietary_vendor_asus-4.4 -b lineage-18.1 vendor/asus
 
+#Sign-Key
 rm -rf vendor/lineage-priv/keys
-git clone https://github.com/sotodrom/keys vendor/lineage-priv/keys
+rm -rf vendor/lineage/signing/keys
+mkdir -p vendor/lineage-priv/keys
+git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
 
 export TZ=Asia/Jakarta
